409a02ed968dec49186a822ae6f5a2d082874257
Julio C. Rocha <julio@rochsquadron.net>
2019-04-20 21:27:42 -0700

    Define playback workflow.

--- TestVlcSharp/MainPage.xaml.cs
+++ TestVlcSharp/MainPage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using LibVLCSharp.Shared;
 using Xamarin.Forms;
 
 namespace TestVlcSharp
@@ -13,9 +16,34 @@ namespace TestVlcSharp
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
+        private LibVLC vlc;
+        private MediaPlayer player;
+
         public MainPage()
         {
             InitializeComponent();
+
+            vlc = new LibVLC();
+            player = new MediaPlayer(vlc);
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (player.IsPlaying)
+                    player.Stop();
+                else
+                {
+                    player.Media = new Media(vlc, PathEntry.Text, FromType.FromPath);
+
+                    player.Play();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Playback Error", ex.StackTrace, "OK");
+            }
         }
     }
 }
