bb7d0421862af022fc6e53996b6ffea68c428488
Julio C. Rocha <julio@rochsquadron.net>
2019-04-20 20:43:46 -0700

    Make Gtk project use shared Forms.

--- TestVlcSharp.Gtk/Program.cs
+++ TestVlcSharp.Gtk/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
-using Gtk;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
 
 namespace TestVlcSharp.Gtk
 {
     class MainClass
     {
+        [STAThread]
         public static void Main(string[] args)
         {
-            Application.Init();
-            MainWindow win = new MainWindow();
-            win.Show();
-            Application.Run();
+            global::Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("LibVLCSharp Test");
+            window.Show();
+
+            global::Gtk.Application.Run();
         }
     }
 }
